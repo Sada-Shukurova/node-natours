c250d67ce1f9336f4a69b9adf23856015359b128
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2024-10-23 02:24:27 +0400

    aggregate pipeline unüind project addFields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -211,7 +211,7 @@ export async function deleteTourById(req, res) {
   }
 }
 
-// AGGREGATION PIPELINE
+// AGGREGATION PIPELINE: match and group
 export async function getTourStats(req, res) {
   try {
     const stats = await Tour.aggregate([
@@ -244,6 +244,57 @@ export async function getTourStats(req, res) {
       status: 'success',
       data: {
         stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+}
+
+// AGGREGATION PIPELINE: unwinding and projecting
+export async function getMonthlyPlan(req, res) {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: { _id: 0 },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
       },
     });
   } catch (err) {
