aa9b7a9f7bc0e2f537376eccce0f0d8c8e1b0004
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2024-10-22 15:34:58 +0400

    adding pagination query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -60,7 +60,20 @@ export async function getAllTours(req, res) {
     } else {
       query = query.select('-__v');
     }
+
+    // PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     const tours = await query;
+
     // console.log(req.query, queryObj);
 
     // const tours = await Tour.find()
