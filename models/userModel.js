5e2d88f921302e1408ab951dc8fe8e24a96dc1b4
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2024-10-30 12:32:21 +0400

    created user Schema and User  model from it

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+    trim: true,
+    // maxlength: [20, 'A user name must have less or equal than 20 characters'],
+    // minlength: [3, 'A user name must have more or equal than 3 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'A password must have more or equal than 8 characters'],
+    // will add some more fields
+    // match: [
+    //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+    //   'Password must contain at least one lowercase letter, one uppercase letter, and one number',
+    // ],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm you parrword'],
+    // validate: {
+    //   validator: function (value) {
+    //     return value === this.password;
+    //   },
+    //   message: 'Passwords do not match',
+    // },
+  },
+  //   createdAt: {
+  //     type: Date,
+  //     default: Date.now(),
+  //     select: false,
+  //   },
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
