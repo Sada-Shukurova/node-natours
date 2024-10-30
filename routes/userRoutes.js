ab8b8c6293a50304bab168246b81d022f3a2547d
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2024-10-30 13:21:33 +0400

    created route for signup User

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,13 @@ import {
   getUserById,
   updateUserById,
 } from '../controllers/userController.js';
+import { signup } from '../controllers/authController.js';
 
 const router = express.Router();
 
+// AUTH
+router.post('/signup', signup);
+// USERS
 router.route('/').get(getAllUsers).post(createUser);
 router
   .route('/:id')
